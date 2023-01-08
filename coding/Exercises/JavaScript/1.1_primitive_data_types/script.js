c6e5b69bffacfb7a489def8c263876477f7c0cfe
sahar118 <sahar.t118@gmail.com>
2023-01-08 11:58:53 +0200

    add files

---
+++ coding/Exercises/JavaScript/1.1_primitive_data_types/script.js
@@ -0,0 +1,27 @@
+Instructions
+// 1. Create a variable whose value holds a string.
+let firstName ='Sahar';
+console.log(firstName);
+
+// 2. Create a variable whose value holds a number.
+let cats =5;
+console.log(cats);
+
+
+// 3. Create a variable whose value holds an object.
+let mathNum =  Math.floor(3.6555);
+console.log(mathNum);
+// 4. Create a variable whose value holds a boolean.
+
+let gameOver= false;
+console.log(gameOver);
+// 5. Create a variable whose value holds null.
+
+let loggedInUser =null;
+
+loggedInUser = 'Sahar Sabek'
+// 6. Create a variable whose value is undefined.
+let myName;
+
+
+// 7. Create a variable whose value holds a symbol.
