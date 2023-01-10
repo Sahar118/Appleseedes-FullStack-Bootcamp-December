4cb63188ddc6f38a98c38d1bf66a396c23420073
sahar118 <sahar.t118@gmail.com>
2023-01-10 13:54:17 +0200

    add files

--- coding/Exercises/JavaScript/5.3_password_validation/script.js
+++ coding/Exercises/JavaScript/5.3_password_validation/script.js
@@ -1,59 +1,29 @@
-
-// function passwordFunc(n) {
-// let num;
-//     for (let num= 0; num <= n; num++){
-//             if((num.length >= 7) && (num.toString().includes('/[A-Z]/')>=1)){
-//                 console.log('Very Strong');
-//             }
-//             else if  ((num.length >= 7) && (num.toString().includes('/[A-Z]/')===0)){
-//                 console.log('Strong');
-//             }
-        
-//         else {
-//                 console.log('Weak');
-//             }
-            
-//         }
-//     };
-// passwordFunc(num='kfHg12365');
+function  passwordFunc1 (password){
+    if (password.length >= 7){
+        console.log('Strong');
+        }
+    else {
+        console.log('Weak');}
+    };
+    passwordFunc1('54155fjhgd')
 
 
-function passwordFunc(n) {
-    let isUpper = /[A-Z]/.test(n);
+    // ********************************
+    
+    let funcPassword =(password) =>{password.length >= 7 ? 'Strong' : 'Weak'};
+    // ********************************
+function passwordFunc5 (password){
+    let isUpper = /[A-Z]/.test(password);
+    if (password.length >= 7){
+           if (isUpper = /[A-Z]/.test(password)){
+            console.log('Very Strong');
+           }
+           else{ console.log('Strong');
+        }
+    }
+    else {
+        console.log('Weak');}
+    };
 
-    let num;
-        for (let num= 0; num <= n; num++){
-                if(num.length >= 7){
-                    if (num.isUpper>=1){
-                    console.log('Very Strong');
-                }
-                   else{ console.log('Strong');
-                }
-            }
-            
-            else {
-                    console.log(num);
-                }
-                
-            }
-        };
-    passwordFunc('kfHg12365');
+    passwordFunc5(`HJDS12355`);
 
-// function divisble7(n){
-//     let num;
-//     for (let num= 1; num <= n; num++) {
-//             if(num % 7 == 0 ){
-//                 if (num.toString().includes('7')){
-//                 console.log('BOOM-BOOM');
-//     }
-//     else{ 
-//             console.log('BOM');
-//         }
-//         }
-//         else {
-//                 console.log(num);
-//             }
-//         }
-//         };
-
-// console.log(divisble7(num=18));
