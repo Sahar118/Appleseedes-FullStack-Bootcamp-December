07dd69a12040a7c904f0faa7007fda3bd4045440
sahar118 <sahar.t118@gmail.com>
2023-01-15 11:54:36 +0200

    add files

---
+++ coding/Exercises/JavaScript/9.2_while_loop/script.js
@@ -0,0 +1,12 @@
+const arrayFunc = ["boo", "doooo", "hoo","ro"];
+
+
+for(let i=0; i<= arrayFunc[i].length; i++ ){
+    console.log(arrayFunc[i]);
+};
+
+let j=0;
+while (j <=arrayFunc.length){
+    console.log(arrayFunc[j]);
+    j++;
+}
