9c47ae6e9018fc8f312b3fa22f6f395bdd2a024b
sahar118 <sahar.t118@gmail.com>
2023-02-05 14:07:41 +0200

    add files

--- coding/Exercises/DOM/25.2_print_name/app.js
+++ coding/Exercises/DOM/25.2_print_name/app.js
@@ -3,8 +3,13 @@ let person = {
     name: 'John',
     getName: function() {
         console.log(this.name);
+        },
+        printAfterSec:function(){
+        const func = person.getName.bind(person);
+        setTimeout(func, 1000);
     }
+    
 };
+console.log(person.getName())
+console.log(person.printAfterSec())
 
-let func = person.getName.bind(person);
-setTimeout(func, 1000);
